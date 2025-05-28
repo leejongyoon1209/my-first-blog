7b84f14fb1901118b6f08c2b2bf76d0b07840d0a
leejongyoon1209 <bidakee11@naver.com>
2025-05-29 00:53:16 +0900

    최종 수정3

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@$5)x!vxy7#hn=6!ax^^jfq&tw@qgz^y$rsafojr2ugozb*bp6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'bidakee.pythonanywhere.com']
 
 
 # Application definition
