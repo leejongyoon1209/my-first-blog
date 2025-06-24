e536971852423a210ec2dd51fb7d4bf87af43ed7
leejongyoon1209 <bidakee11@naver.com>
2025-06-24 19:23:27 +0900

    최종수정

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('api_root/', include(router.urls)),
+    path('js_test/', views.js_test, name='js_test'),
 ]
